f0c0c0d0cd7488f2380c65b9fa641971f5c38365
Carolista <cjones.renaissancewoman@gmail.com>
2025-10-08 17:56:57 -0400

    updated fetching-JSON modern-syntax demo

--- fetching-JSON/modern-syntax/script.js
+++ fetching-JSON/modern-syntax/script.js
@@ -1,54 +1,84 @@
-/*** Pet Pics ***/
+import { getRandomElement, getTitleCase } from './shared/utils.js';
+
+/* Put shared HTML on all pages */
+const url = new URL(window.location.href);
+const pathNameArray = url.pathname.split('/');
+const fileName = pathNameArray[pathNameArray.length - 1];
+const pageName = fileName.slice(0, fileName.indexOf('.'));
+const titleText = pageName == 'index' ? 'Welcome' : getTitleCase(fileName);
+const navPath = pageName == 'index' ? '/pages' : '';
+const homePath = pageName == 'index' ? '' : '../index.html';
+const head = document.querySelector('head');
+const title = document.querySelector('title');
+
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const thisYear = new Date().getFullYear();
+
+title.innerText = `Main Street Pet Rescue | ${titleText}`;
+head.innerHTML += `<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Damion&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    `;
+
+header.innerHTML = `<h1>Main Street Pet Rescue</h1>
+    <nav>
+        <li class="nav-link"><a href="${homePath}">Home</a></li>
+        <li class="nav-link"><a href=".${navPath}/adopt.html">Adopt</a></li>
+        <li class="nav-link"><a href=".${navPath}/volunteer.html">Volunteer</a></li>
+        <li class="nav-link"><a href=".${navPath}/donate.html">Donate</a></li>
+        <li class="nav-link"><a href=".${navPath}/location.html">Location</a></li>
+    </nav>`;
+
+footer.innerHTML = `
+    <div>Main Street Pet Rescue | 987 Main Street | St. Louis, MO 63101</div>
+    <div>&copy; ${thisYear} Friends of MSPR</div>
+    `;
 
 /*
-    Some practice on using fetch - utilizing two different public APIs (linked below)
+    Fetch photos URLs from two different public APIs (linked below)
 */
 
-const result = document.getElementById('result');
+const photoSection = document.getElementById('photo-section');
+const photoUrls = [];
 
-// Event delegation - listen for click on entire document, then check id of target
-document.addEventListener('click', function (event) {
-	// RANDOM DOG PIC courtesy of https://dog.ceo/dog-api/
-	// Use the API documentation to look up the correct endpoint for a random photo
-	if (event.target.id === 'dog-button') {
-		const fetchDogData = async () => {
-			let response = await fetch('https://dog.ceo/api/breeds/image/random');
-			let data = await response.json();
-
-			// console.log(data);
-
-			result.innerHTML = `<img src="${data.message}" width="300px" />`;
-		};
-
-        // Call async function
-        fetchDogData();
-	}
-
-	// RANDOM CAT PIC courtesy of https://thecatapi.com/
-	// Use the API documentation to look up the correct endpoint for 5 random photos
-	// Bonus: Narrow the breed to Norweigan Forest Cat
-	// You will need to request a free key
-	// Note: a header will be required!
-	// Display all five images in the result section.
-	if (event.target.id === 'cat-button') {
-		async function getCatPic() {
-			let response = await fetch('https://api.thecatapi.com/v1/images/search?limit=5&breed_ids=norw', {
-				headers: {
-					'x-api-key':
-						'live_OGxGd4Qnu16oAEBlonS9roxlp8vKLHd0CNZ7KHIE67CqGGGBPJdg8FSbWEgMMoNA',
-				},
-			});
-			let data = await response.json();
-
-			// console.log(data);
-
-			result.innerHTML = ''; // Clear any previous photos
-            for (let obj of data) {
-                result.innerHTML += `<img src="${obj.url}" width=300px />`
-            }
+// RANDOM CAT PICS courtesy of https://thecatapi.com/
+// Fetch 20 random photos
+async function getCatPics() {
+	let response = await fetch(
+		'https://api.thecatapi.com/v1/images/search?limit=20',
+		{
+			headers: {
+				'x-api-key':
+					'live_OGxGd4Qnu16oAEBlonS9roxlp8vKLHd0CNZ7KHIE67CqGGGBPJdg8FSbWEgMMoNA',
+			},
 		}
+	);
+	let data = await response.json();
 
-		// Call your async function to execute the fetch
-		getCatPic();
+	console.log(data);
+
+	// Add all urls to photoUrls array (see logged data for structure)
+	for (let obj of data) {
+		photoUrls.push(obj.url);
 	}
-});
+
+    // Put images on page
+	displayRandomPhotos();
+}
+
+function displayRandomPhotos(amount = 1) {
+    const randomUrls = [];
+    let randomUrl = getRandomElement(photoUrls);
+	while (randomUrls.length < amount && !randomUrls.includes(randomUrl)) {
+        randomUrls.push(randomUrl);
+		randomUrl = getRandomElement(photoUrls);
+	}
+    for (let url of randomUrls) {
+        photoSection.innerHTML += `<img src="${url}" maxwidth="400" />`;
+    }
+}
+
+// Call both async functions to execute the fetch code
+getCatPics();
+fetchDogPics();
